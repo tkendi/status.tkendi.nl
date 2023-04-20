d68437211729531ef9f694709fd52a5236bc557f
ChayMyungjune <acg70159@gmail.com>
2023-04-20 21:51:50 +0900

    [FEAT] Feat Head Title Create

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
-import { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
-
 // styles
 import '@/styles/global.css';
 
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
+
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <ThemeProvider defaultTheme="system" attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Tkendi Status</title>
+      </Head>
+      <ThemeProvider defaultTheme="system" attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 };
 
